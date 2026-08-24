91dd1afbf65dd182242d5e9a5fbaed9b356c4c08
djahern-max <danielaherniv@gmail.com>
2026-08-24 09:48:59 -0400

    video: lesson 03 narration and module

--- video/src/lessons.ts
+++ video/src/lessons.ts
@@ -1,5 +1,6 @@
 import * as lesson01 from "./lesson-01";
 import * as lesson02 from "./lesson-02";
+import * as lesson03 from "./lesson-03";
 
-export const LESSONS = { "01": lesson01, "02": lesson02 } as const;
+export const LESSONS = { "01": lesson01, "02": lesson02, "03": lesson03 } as const;
 export type LessonId = keyof typeof LESSONS;
